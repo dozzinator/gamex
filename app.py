2fdf4eb3eaee5fcd379fcd108f94e826be1938bb
dozzinator <laurens.verwaaijen@gmail.com>
2021-01-06 13:16:49 +0100

    second init

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+
+# Database
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['user_login_system']
+
+# Routes
+from user import routes
 
 @app.route('/')
 def home():
